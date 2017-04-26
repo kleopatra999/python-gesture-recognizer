f27e45e6efedf30a80fc0378dbe8881f95900160
Gilbert Röhrbein <gilbert@ifsr.de>
2017-04-26 21:32:52 +0200

    added viwi_gestures_client, takes touch events and produces VIWI touches and touchgroups and POSTs them to a gestures service

---
+++ viwi_gestures_client.py
@@ -0,0 +1,32 @@
+import requests, json
+
+PORT_GESTURES = 49217
+
+def touch_event( type_, id_, timestamp, x, y ):
+    print( "touch_event", type_, id_, timestamp, x, y )
+    touch = {
+            "clientX": int(x * 600),
+            "clientY": int(y * 400),
+    }
+    
+    print( "POST touch", touch )
+    response = requests.post(
+            "http://127.0.0.1:49217/gestures/touches/",
+            data = json.dumps( touch ))
+    print( "response", response.headers, response.text )
+
+    touch["id"] = response.headers["location"]
+    touch["url"] = "/gestures/touches/" + touch["id"]
+
+    touchgroup = {
+            "timeStamp": int(timestamp),
+            "type": str(type_),
+            "touches": [ touch["id"] ]
+    }
+
+    print( "POST touchgroup", touchgroup )
+    response = requests.post(
+            "http://127.0.0.1:49217/gestures/touchgroups/",
+            data = json.dumps( touchgroup ))
+    print( "response", response.headers, response.text )
+
